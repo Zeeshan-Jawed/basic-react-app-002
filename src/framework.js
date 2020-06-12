d528bd3b3c3d5f044e13f45f0c612b875711e2b9
Zeeshan-Jawed <jawedzeeshan1@gmail.com>
2020-06-12 11:40:30 +0500

    add framework.js

---
+++ src/framework.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Framework(props){
+    return (
+    <div>
+        <h1>{props.library} is Library</h1>
+        <h1>{props.frameWork} is Framework</h1>
+    </div>
+    )
+}
+export default Framework;
